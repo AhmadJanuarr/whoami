e8f9ffeb0b4cc61d35c3f86ecd330079ccd04528
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-12-03 15:38:15 +0700

    fix: update Tailwind CSS content paths to reflect new directory structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss"
 
 const config: Config = {
   darkMode: ["class"],
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: ["./src/_app/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       perspective: {
